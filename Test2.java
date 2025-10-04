23be97c577a8a8b713f7dd0779f73de1fbbf1049
pinkannanda <pinkannanda21@gmail.com>
2025-10-04 18:24:46 +0700

    collab

--- Test2.java
+++ Test2.java
@@ -1,5 +1,8 @@
 public class Test2 {
     public static void main(String[] args) {
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");
+        System.out.println("User 1 menambahkan baris baru");
+
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2");
         System.out.println("User 2 menambahkan baris baru");
     }
